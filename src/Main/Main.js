4825666d91bae7f8325928af2e507a1f147821eb
serdalaydin6116 <serdalaydin.6116@gmail.com>
2022-04-05 00:58:10 +0300

    first commit

---
+++ src/Main/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from '../Card/Card'
+import Button from '../Button/Button'
+import '../Card.css'
+<Card className="css"></Card>
+
+const Main = () => {
+  return (
+    <div>
+      <Card />
+      <Button />
+        
+    </div>
+  )
+}
+
+export default Main
